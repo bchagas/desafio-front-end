1b6e9e7cfb1ba527a27d0b9907d4c5ee306b3dff
Bruno Chagas <brunochagasweb@gmail.com>
2017-03-14 23:30:45 -0300

    Fixed pre search when user type address

--- src/app/search-input/search-input.component.ts
+++ src/app/search-input/search-input.component.ts
@@ -1,37 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { SearchService } from '../services/search.service';
 
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/of';
+
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.css']
 })
-export class SearchInputComponent implements OnInit {
+export class SearchInputComponent {
   @Input() placeholder: string;
 
   address = new FormControl();
-  items: Observable<Array<string>>;
+  displaySearchResults = false;
+  addresses: Observable<Array<string>>;
 
   constructor(
     private searchService: SearchService
-  ) {}
-
-  ngOnInit() {
-    this.address.valueChanges
-        .debounceTime(600)
-        .distinctUntilChanged()
-        .switchMap(term => this.searchService.search(term))
+  ) {
+    this.searchService
+        .search(this.address.valueChanges)
         .subscribe(response => {
-          console.log(response);
-        }, (error) => {
-          console.log(error);
+          const resultAddresses = [];
+          response.results.map(address => {
+            resultAddresses.push(address.displayName);
+          });
+          this.addresses = Observable.of(resultAddresses);
+          this.displaySearchResults = true;
         });
+  }
+
+  setAddress(event) {
+    this.address.setValue(event.target.textContent);
+    this.displaySearchResults = false;
   }
 }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/mergeMap';
 
 @Injectable()
 export class SearchService {
@@ -8,7 +13,12 @@ export class SearchService {
     private api: ApiService
   ) { }
 
-  search(address: string) {
-    return this.api.get('/search', address).map(response => response.json());
+  search(address: Observable<string>) {
+    return address.debounceTime(600)
+           .distinctUntilChanged()
+           .flatMap(addr =>
+              this.api.get('/search', addr)
+                      .map(response => response.json())
+            );
   }
 }
